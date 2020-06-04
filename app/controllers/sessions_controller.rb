24e7d564e1ba9fdf8194e8559c88f8726aaac20a
Matthew <mattcorralejo1@gmail.com>
2020-06-04 09:43:35 -0700

    create method added in sessionscontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,13 @@ class SessionsController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(password: params[:user][:password])
+            sessions[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+    end
 end
