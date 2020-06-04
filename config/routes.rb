7eae739a183c4e9213d48129dc2027b813064f53
Matthew <mattcorralejo1@gmail.com>
2020-06-04 06:34:47 -0700

    adding signup and login  routes in config folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :users
+  resources :users, except: [:new]
   resources :vehicles
+
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
