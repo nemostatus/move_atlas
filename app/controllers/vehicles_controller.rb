e1897bc6873a4fafbf3df590f51768fddf34709c
Matthew <mattcorralejo1@gmail.com>
2020-06-05 03:26:16 -0700

    strong params for vehicle controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -4,5 +4,13 @@ class VehiclesController < ApplicationController
     end
      
     def create
+        @vehicle = Vehicle.new(vehicle_params)
     end
+
+    private
+    def vehicle_params
+        params.require(:vehicle).permit(:plate_number, 
+        :plate_state, :vehicle_type, :pick_up_date, :company_name, :status, :bug_type)
+   
+    end 
 end
