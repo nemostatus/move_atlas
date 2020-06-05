691b3355cbe22c80d832b310c6e14b24a8c627cc
Matthew <mattcorralejo1@gmail.com>
2020-06-05 05:13:21 -0700

    updating vehicles controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,21 +1,30 @@
 class VehiclesController < ApplicationController
+    def index
+        @vehicles = Vehicle.all
+    end
     def new
         @vehicle = Vehicle.new
+        @vehicle.reviews.build(params[:reviews_attributes])
     end
      
     def create
         @vehicle = Vehicle.new(vehicle_params)
         if @vehicle.save
-            redirect_to @vehicle
+            redirect_to vehicles_path(@vehicle)
         else
             render :new
         end 
     end
 
+    def show
+        @vehicle = Vehicle.find(params[:id])
+         
+    end 
+
     private
     def vehicle_params
         params.require(:vehicle).permit(:plate_number, 
-        :plate_state, :vehicle_type, :pick_up_date, :company_name, :status, :bug_type)
+        :plate_state, :vehicle_type, :pick_up_date, :company_name, :status, :bug_type, reviews_attributes: [:customer_experience_rating])
    
     end 
 end
