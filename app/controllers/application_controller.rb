8825398e5cd3262fa92259c58e71bad829b89eb2
Matthew <mattcorralejo1@gmail.com>
2020-06-04 20:48:45 -0700

    creating signup and create method in users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
     def home
         redirect_to login_path
     end
+    def current_user    
+        User.find_by(id: session[:user_id])  
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(username: params[:user][:username])
+        @user = User.find_by(username: params[:user][:username]) #create signup  so it has something to find
         if @user && @user.authenticate(password: params[:user][:password])
             sessions[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
     end
+end
 end
