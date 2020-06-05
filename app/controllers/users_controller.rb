bc40d69cd9c13589e05773fadc99e840d380c47c
Matthew <mattcorralejo1@gmail.com>
2020-06-04 21:43:13 -0700

    adjusting routes for welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create 
       @user = User.create(params.require(:user).permit(:username, :password))
       session[:user_id] = @user.id 
-      redirect_to user_path(@user)
+      redirect_to '/welcome'
     end 
 
     def show 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'application#home'
   resources :reviews
-  resources :users, except: [:new]
+  resources :users, only: [:new, :create]
   resources :vehicles
-
+  get '/welcome', to: 'sessions#welcome'
   get '/signup', to: 'users#new'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
+
   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
