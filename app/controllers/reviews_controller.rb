df063ad0b438eee8b6fd7fbede369268174d7e0b
Matthew <mattcorralejo1@gmail.com>
2020-06-12 18:57:59 -0700

    starting to DRY starting with reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,10 @@
 class ReviewsController < ApplicationController
-  def index
+
+     def index
      render :index
       
-  end 
-
-  def new
-    @vehicle= Vehicle.find(params[:vehicle_id])
-    @review = Review.new
-  end
-
-  def create
-    @vehicle= Vehicle.find(params[:vehicle_id])
-      @review = Review.new(review_params)
-      @review.vehicle = @vehicle 
-      if @review.save
-       redirect_to reviews_path(@review)
-       else
-         render :new
-        end 
-      end
-
+     end 
+    
       def edit
          
         @vehicle= Vehicle.find(params[:vehicle_id])
@@ -35,23 +20,20 @@ class ReviewsController < ApplicationController
               render :edit
             end
             end
-  def show
-      @review = Review.find(params[:id])
-      @vehicle = @review.vehicle 
-      render :show 
-  end 
+
   def destroy
-    @vehicle= Vehicle.find(params[:vehicle_id])
-    @review = Review.find(params[:id])
+    
       if @review.destroy
       redirect_to user_path(current_user)
       else
         render :show
   end
 end
+
   private
   def review_params
   params.require(:review).permit(:customer_experience_rating, :vehicle_id)
-end
+  end
+
 end
 
