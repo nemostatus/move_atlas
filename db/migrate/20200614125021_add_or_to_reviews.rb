1557664151cfb869844afc9f7dbe466a831b0fd7
Matthew <mattcorralejo1@gmail.com>
2020-06-16 13:21:56 -0700

    adding created by in reviews index need to add conditionals

--- db/migrate/20200614125021_add_or_to_reviews.rb
+++
@@ -1,5 +0,0 @@
-class AddOrToReviews < ActiveRecord::Migration[6.0]
-  def change
-    add_column :reviews, :overall_rating, :integer
-  end
-end
--- db/migrate/20200614144236_remove_column_from_reviews.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromReviews < ActiveRecord::Migration[6.0]
-  def change
-   remove_column :reviews, :overall_rating, :integer
-  end
-end
