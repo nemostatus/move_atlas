b5893ad1bccad721dbdb10f02f6444c3e59393da
Matthew <mattcorralejo1@gmail.com>
2020-06-21 13:53:10 -0700

    directions added on homepage for github signup/sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
      if user.save
        session[:user_id] = user.id 
           redirect_to root_path
-     else 
+     else
       redirect_to root_path
-      end
+     end
     end
 
 
