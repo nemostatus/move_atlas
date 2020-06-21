9acf72bb300957508436c4dce633c19fdda6ada1
Matthew <mattcorralejo1@gmail.com>
2020-06-21 13:23:29 -0700

    added conditional to verify user is saved need to start testing and being thorough with spec.md

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,14 @@ class SessionsController < ApplicationController
     
     def oauth
      user = User.find_or_create_by_omniauth(auth_hash)
+     if user.save
        session[:user_id] = user.id 
           redirect_to root_path
+     else 
+      redirect_to root_path
       end
-  
+    end
+
 
     def new
         @user = User.new
