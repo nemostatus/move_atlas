a0a206cd8c63cb3fc4a8fe89e9371a5908d6e72e
Matthew <mattcorralejo1@gmail.com>
2020-06-21 03:48:21 -0700

    autocomplete off

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
 
-    def home
-        redirect_to welcome_path
-    end
+   
     def current_user    
         @current_user ||= User.find_by(id: session[:user_id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'application#home'
+  root to: 'sessions#welcome'
 
   resources :users, only: [:new, :create, :show]
  
