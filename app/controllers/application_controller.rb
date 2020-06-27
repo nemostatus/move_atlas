5338b44ae13e35387628736b6dded20488642688
Matthew <mattcorralejo1@gmail.com>
2020-06-27 09:24:50 -0700

    fixing redirect in oauth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,16 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def logged_in?	
-    !current_user.nil?	
-  end	
+  def logged_in?
+    !current_user.nil?
+  end
 
-  def log_in(user)	
-    session[:user_id] = user.id	
-  end	
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-  def log_out	
-    session[:user_id] = nil	
-    redirect_to "/", notice: "You are logged out!"	
-  end	
+  def log_out
+    session[:user_id] = nil
+    redirect_to "/", notice: "You are logged out!"
+  end
 end
-
-
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_or_create_by_omniauth(auth_hash)
     if user.save
       session[:user_id] = user.id
-      redirect_to root_path
-    else
       redirect_to user_path(user)
+    else
+      redirect_to root_path
     end
   end
 
